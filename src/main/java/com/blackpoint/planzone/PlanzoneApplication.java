6e0856729ee041ca24674dd70ec07672db5c26b9
blackpointdev <marcinw1998@gmail.com>
2021-09-21 16:27:11 +0200

    Initial commit

---
+++ src/main/java/com/blackpoint/planzone/PlanzoneApplication.java
@@ -0,0 +1,13 @@
+package com.blackpoint.planzone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanzoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanzoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blackpoint/planzone/PlanzoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackpoint.planzone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanzoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
